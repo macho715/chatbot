6941539749d81f094fefff686c5672a9a7ac02d4
macho715 <132314278+macho715@users.noreply.github.com>
2025-08-12 23:46:33 +0400

    [FIX] PostCSS 설정 단순화 - 빌드 오류 해결

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,5 @@ module.exports = {
   plugins: {
     'tailwindcss': {},
     'autoprefixer': {},
-    'postcss-preset-env': {
-      features: {
-        'nesting-rules': false,
-      },
-    },
   },
 }
